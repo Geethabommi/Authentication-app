e7856f9e26ec6503d9419eabf6ad2a72a1e19104
Geethabommi <geethabommi139@gmail.com>
2022-06-02 12:28:52 +0530

    port env change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
@@ -76,10 +76,10 @@ app.use(customMware.setFlash);
 // use express router
 app.use('/', require('./routes'));
 
-app.listen(port, function (err) {
+app.listen(PORT, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);
   }
 
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
